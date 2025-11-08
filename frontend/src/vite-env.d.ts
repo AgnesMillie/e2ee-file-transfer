a3514ba1bc4e3f09e08bb430bca5a1027841efcf
violetcannary <daycaremoon22@gmail.com>
2025-11-07 21:28:26 -0300

    feat(frontend): add api base url via docker-compose

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+// Informa ao TypeScript sobre as variáveis VITE_
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
